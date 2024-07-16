03bbcd5cd7ef5adfe16c6e8334498633c3876c14
Geuber Lucas <geuberlu@gmail.com>
2024-07-15 21:06:48 -0300

    feat(app/routes): Add home and simulation routes to app-routing.module.ts feat(home/html): add divs and text in page feat(home/css): add styles for page

--- FrontEnd/SimpleRent/src/app/app-routing.module.ts
+++ FrontEnd/SimpleRent/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { SimulationFormComponent } from './features/simulation-form/simulation-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'simulation',component:SimulationFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/SimpleRent/src/app/core/core.module.ts
+++ FrontEnd/SimpleRent/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   exports: [NavbarComponent,
     PageNotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class CoreModule { }
